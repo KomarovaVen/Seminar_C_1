f8dca674d9ea5707eb84581325270e7c066ade2b
v.komarova <venerakomarova@gmail.com>
2023-06-28 09:42:53 +0400

    HomeWork_2 zadanie 2

--- Lesson_2/HomeWork_2/Zadanie_2/Program.cs
+++ Lesson_2/HomeWork_2/Zadanie_2/Program.cs
@@ -1,12 +1,17 @@
 ﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
 
-int TakeNum(int num)
+
+int Number = new Random().Next(100, 1000);
+int NewNumber = 0;
+
+int NumberWithoutSecDig()
 {
-    Console.WriteLine(num);
-    return num % 100 / 10;
+    NewNumber = Number / 100 * 10 + Number % 10;
+    return NewNumber;
 }
 
+NumberWithoutSecDig();
 
-int result = TakeNum(new Random().Next(100, 1000));
-
-Console.WriteLine(result);
+System.Console.WriteLine();
+System.Console.WriteLine($"Если из числа {Number} удалить вторую цифру, получится число {NewNumber}");
+System.Console.WriteLine();
