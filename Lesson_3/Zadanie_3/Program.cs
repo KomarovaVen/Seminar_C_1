519f1d461fd7002be1599ffeccd4e0d5a90e5ddb
v.komarova <venerakomarova@gmail.com>
2023-07-03 15:09:45 +0400

    Lesson_3 HomeWork3 z3

--- Lesson_3/Zadanie_3/Program.cs
+++ Lesson_3/Zadanie_3/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+void Squares(int num)
+{
+    int i = 1;
+    Console.Write($"{num} -> ");
+
+    while (num >= i)
+    {
+        Console.Write($"{Math.Pow(i, 2)}, ");
+        i++;
+    }
+    Console.WriteLine();
+}
+
+int num = int.Parse(Console.ReadLine()!);
+Squares(num);
