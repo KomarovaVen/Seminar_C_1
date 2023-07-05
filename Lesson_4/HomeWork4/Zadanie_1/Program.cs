0dd55bc3ab4b3fdbef6d1e427ba4f6d1fd9aa973
v.komarova <venerakomarova@gmail.com>
2023-07-05 15:33:50 +0400

    Lesson_4 HomeWork3 z1

---
+++ Lesson_4/HomeWork4/Zadanie_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.WriteLine("Введите число:");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите степень:");
+int b = int.Parse(Console.ReadLine()!);
+int res = 0;
+var i = 1;
+
+
+for (i = 1; i <= a; i++)
+    res = a *= b;
+Console.WriteLine(res);
+
