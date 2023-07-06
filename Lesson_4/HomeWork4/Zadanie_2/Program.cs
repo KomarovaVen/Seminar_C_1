2917a39c8831089eabfcf847a124ff8438df667b
v.komarova <venerakomarova@gmail.com>
2023-07-06 14:31:19 +0400

    Lesson_4 HomeWork4 z2

--- Lesson_4/HomeWork4/Zadanie_2/Program.cs
+++ Lesson_4/HomeWork4/Zadanie_2/Program.cs
@@ -2,3 +2,18 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+int SumNum(int num)
+{
+    int all_sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        all_sum += num % 10;
+        num /= 10;
+    }
+    return all_sum;
+}
+Console.WriteLine("Введите число:");
+string answer = Console.ReadLine()!;
+int ans = answer == null ? 0 : int.Parse(answer);
+Console.WriteLine(SumNum(ans));
+
