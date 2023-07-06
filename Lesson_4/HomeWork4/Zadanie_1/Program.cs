cfa524ca5a9799b72286ed37c43735a4ae02f6d5
v.komarova <venerakomarova@gmail.com>
2023-07-06 11:51:08 +0400

    Lesson_4 HomeWork3 z1

--- Lesson_4/HomeWork4/Zadanie_1/Program.cs
+++ Lesson_4/HomeWork4/Zadanie_1/Program.cs
@@ -6,10 +6,11 @@ int a = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите степень:");
 int b = int.Parse(Console.ReadLine()!);
 int res = 0;
-var i = 1;
+int i = 1;
 
 
-for (i = 1; i <= a; i++)
-    res = a *= b;
-Console.WriteLine(res);
-
+for (i = 1; i <= b; i++)
+    res = a * b;
+{ Console.WriteLine(res); }
+if (res == 0)
+{ Console.WriteLine("Нельзя умножить на 0"); }
