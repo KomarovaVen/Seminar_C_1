7c570a08f8269eca38512de5cc6c640f8424e9f1
v.komarova <venerakomarova@gmail.com>
2023-07-11 19:46:01 +0400

    6 Lesson demo

--- Lesson_6/demo/Program.cs
+++ Lesson_6/demo/Program.cs
@@ -1,15 +1,14 @@
-﻿// Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
-// [1 2 3 4 5] -> [5 4 3 2 1]
-// [6 7 3 6] -> [6 3 7 6]
+﻿// See https://aka.ms/new-console-template for more information
+// 1. Напишите программу, которая перевернёт одномерный массив
+//    (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
 
 void Print(int[] arr)
 {
     int size = arr.Length;
 
     for (int i = 0; i < size; i++)
-    {
         Console.Write($"{arr[i]} ");
-    }
     Console.WriteLine();
 }
 
@@ -18,29 +17,26 @@ int[] MassNums(int size, int from, int to)
     int[] arr = new int[size];
 
     for (int i = 0; i < size; i++)
-    {
         arr[i] = new Random().Next(from, to + 1);
-    }
+
     return arr;
 }
 
-void ArrRev(int[] arr)
+void RevMas(int[] arr_1)
 {
-    int size = arr.Length;
+    int size = arr_1.Length;
 
     for (int i = 0; i < size / 2; i++)
-        (arr[i], arr[size - i - 1]) = (arr[size - i - 1], arr[i]);
-
+        (arr_1[i], arr_1[size - i - 1]) = (arr_1[size - i - 1], arr_1[i]);
 }
 
-Console.WriteLine("Задайте размерность массива ");
+
+
 int num = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Задайте начальный  диапозон массива ");
 int start = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Задайте кнечный диапозон массива ");
 int stop = int.Parse(Console.ReadLine()!);
 
 int[] mass = MassNums(num, start, stop);
 Print(mass);
-ArrRev(mass);
+RevMas(mass);
 Print(mass);
