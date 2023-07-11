6c350452f6c10dd6e34d4410890da729fe685dbb
KomarovaVen <136476322+KomarovaVen@users.noreply.github.com>
2023-07-11 19:36:17 +0400

    Update Program.cs

--- Lesson_5/HomeWork5/Zadanie3/Program.cs
+++ Lesson_5/HomeWork5/Zadanie3/Program.cs
@@ -40,9 +40,9 @@ void ChisloRaznica(double[] arr)
 
 Console.WriteLine("Задайте размерность массива ");
 int num = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Задайте начальный 3-х значный диапозон массива ");
+Console.WriteLine("Задайте начальный диапозон массива ");
 int start = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Задайте кнечный 3-х значный диапозон массива ");
+Console.WriteLine("Задайте кнечный диапозон массива ");
 int stop = int.Parse(Console.ReadLine()!);
 
 double[] mass = MassNums(num, start, stop);
