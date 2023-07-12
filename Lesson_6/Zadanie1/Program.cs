681d3c03d59795067d4b50d9e00ea7d45bd74ff0
v.komarova <venerakomarova@gmail.com>
2023-07-12 13:15:27 +0400

    6 Lesson z1

--- Lesson_6/Zadanie1/Program.cs
+++ Lesson_6/Zadanie1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу, которая принимает на вход три числа
+//    и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+
+void ExiTri(int a, int b, int c)
+{
+    if (a < b + c && b < a + c && c < b + a)
+        Console.WriteLine("Треугольник существует.");
+    else
+        Console.WriteLine("Треугольник не существует.");
+}
+
+// 1 2 3, 4 2 5
+Console.WriteLine("Введите a");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите b");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите c");
+int c = int.Parse(Console.ReadLine()!);
+ExiTri(a, b, c);
