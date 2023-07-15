726d3822a4a0566279e0540419bd23be3795ac0d
v.komarova <venerakomarova@gmail.com>
2023-07-15 13:09:42 +0400

    7 Lesson Urok

---
+++ Lesson7/zadanie1/Program.cs
@@ -0,0 +1,75 @@
+﻿
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to + 1);
+    return arr;
+}
+
+int[,] CopyMass(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+    int[,] new_arr = new int[row_size, column_size];
+    for (int i = 0; i < row_size; i++)
+        for (int j = 0; j < column_size; j++)
+            new_arr[i, j] = arr[i, j];
+    return new_arr;
+}
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the min number of massive ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the max number of massive ");
+int stop = int.Parse(Console.ReadLine()!);
+int[,] mass = MassNums(row_num, column_num, start, stop);
+Print(mass);
+int[,] arr_new = CopyMass(mass);
+Print(arr_new);
+
+//Пользователь вводит с клавиатуры M чисел. Посчитайте,
+//сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Кол-во строк ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Кол-во столбцов ");
+int col = int.Parse(Console.ReadLine()!);
+int[,] matrix = new int[row, col];
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        { Console.Write($"{matrix[i, j]} "); }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        { matrix[i, j] = i + j; }
+    }
+}
+FillArray(matrix); PrintArray(matrix);
---
+++ Lesson7/zadanie3/Program.cs
@@ -0,0 +1,50 @@
+﻿// 3. Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        { System.Console.Write($"{matr[i, j]} "); }
+        System.Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        { matr[i, j] = new Random().Next(1, 10); }
+    }
+}
+
+void Sum_massive(int[,] array)
+{
+    int sum = 0;
+    if (array.GetLength(0) != array.GetLength(1))
+    {
+        Console.WriteLine("не будет ровной диагонали");
+        return;
+    }
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum = sum + array[i, j];
+            }
+        }
+    }
+    Console.Write($"{sum}");
+}
+Console.WriteLine("Введите количество строк ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов ");
+int n = int.Parse(Console.ReadLine()!);
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+PrintArray(matrix);
+System.Console.WriteLine();
+Sum_massive(matrix);
+//PrintArray(matrix);
