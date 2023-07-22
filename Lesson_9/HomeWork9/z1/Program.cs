6650fac5947bfd636782292d2cc689543443dcae
v.komarova <venerakomarova@gmail.com>
2023-07-22 09:51:37 +0400

    9_Lesson HW9

---
+++ Lesson_9/HomeWork9/z1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные
+//натуральные числа в промежутке от M до N с помощью рекурсии.
+//Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine()!);
+void Rez(int m, int n)
+{
+    if (m > n)
+        return;
+    if (m % 2 == 0)
+    {
+        Console.Write($"{m}. ");
+    }
+    Rez(m + 1, n);
+
+}
+Rez(m, n);
+
---
+++ Lesson_9/HomeWork9/z2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму
+//натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+Console.WriteLine("Введите m");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите n");
+int n = int.Parse(Console.ReadLine()!);
+Sum(m, n);
+
+void Sum(int m, int n)
+{
+    Console.Write(Summa(m - 1, n));
+}
+
+int Summa(int m, int n)
+{
+    int rez = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        rez = m + Summa(m, n);
+        return rez;
+    }
+}
